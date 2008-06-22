9a1f7e8547ad312ee7ad08722e6e117e599ff52a
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-06-22 03:06:57 +0000

    added the first real unit test to aspNETserve :-)

---
+++ UnitTests/AspNetWorkerTests.cs
@@ -0,0 +1,31 @@
+﻿/************************************************************************
+ * Copyright (c) 2006-2008, Jason Whitehorn (jason.whitehorn@gmail.com)
+ * All rights reserved.
+ * 
+ * Source code and binaries distributed under the terms of the included
+ * license, see license.txt for details.
+ ************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using aspNETserve.Core;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace UnitTests {
+    [TestFixture]
+    public class AspNetWorkerTests {
+
+        [Test]
+        [Description("Determines that the GetAppPath method of the AspNetWorker IAspNetWorker implementation returns the virtual path provided on the constructor.")]
+        public void GetAppPath_Test() {
+            MockRepository mocks = new MockRepository();
+            string virtualPath = @"/";
+
+            IAspNetRuntime aspNetRuntime = mocks.CreateMock<IAspNetRuntime>();
+            AspNetWorker aspNetWorker = new AspNetWorker(aspNetRuntime, virtualPath, @"z:\temp");
+
+            Assert.AreEqual(virtualPath, aspNetWorker.GetAppPath());
+        }
+    }
+}
