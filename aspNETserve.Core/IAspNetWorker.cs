47eeb9448185b5f4ea99dd63a65bd37fab7218a1
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-06-28 03:45:46 +0000

    added many methods from HttpWorkerRequest to the IAspNetWorker interface.

--- aspNETserve.Core/IAspNetWorker.cs
+++ aspNETserve.Core/IAspNetWorker.cs
@@ -17,5 +17,46 @@ namespace aspNETserve.Core {
     public interface IAspNetWorker {
         void ProcessTransaction(ITransaction transaction);
         IDictionary<string, string> ServerVariables { set; get; }
+
+        /// <summary>
+        /// The virtual directory server configuration element.
+        /// </summary>
+        string GetAppPath();
+        /// <summary>
+        /// The physical path to the web root.
+        /// </summary>
+        string GetAppPathTranslated();
+        /// <summary>
+        /// The current executing AppDomain's Id
+        /// </summary>
+        string GetAppPoolID();
+        /// <summary>
+        /// The numbers of bytes posted
+        /// </summary>
+        long GetBytesRead();
+        /// <summary>
+        /// Allways returns 0
+        /// </summary>
+        long GetConnectionID();
+        /// <summary>
+        /// The virtual file path
+        /// </summary>
+        string GetFilePath();
+        /// <summary>
+        /// Returns the path on disk for the current URI.
+        /// </summary>
+        /// <remarks>
+        /// If the URI lacks a file name, and instead just specifies a directory, 
+        /// then the result of GetFilePathTranslated will just be the directory.
+        /// </remarks>
+        string GetFilePathTranslated();
+        /// <summary>
+        /// Gets the HTTP verb for the current request.s
+        /// </summary>
+        string GetHttpVerbName();
+        /// <summary>
+        /// Gets the version of the HTTP protocol used for communications with the client.
+        /// </summary>
+        string GetHttpVersion();
     }
 }
