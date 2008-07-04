214b07f0a824a5294e22f7956f42bd37f2849842
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-04 04:32:30 +0000

    added the first unit test for the HttpRequestResponse object.

---
+++ UnitTests/HttpRequestResponseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using aspNETserve;
+using NUnit.Framework;
+
+namespace UnitTests {
+    [TestFixture]
+    public class HttpRequestResponseTests {
+
+        [Test]
+        public void Simple_Get_Request_Test() {
+            string httpRequest =
+                @"GET / HTTP/1.1
+Host: Localhost
+
+";
+            MemoryStream stream = new MemoryStream(Encoding.ASCII.GetBytes(httpRequest));
+            HttpRequestResponse httpRequestResponse = new HttpRequestResponse(stream, null, null);
+
+            Assert.AreEqual(httpRequestResponse.Request.HttpMethod, "GET");
+            Assert.AreEqual(httpRequestResponse.Request.RawUrl, "/");
+            Assert.IsFalse(httpRequestResponse.Request.IsKeepAlive);
+        }
+    }
+}
