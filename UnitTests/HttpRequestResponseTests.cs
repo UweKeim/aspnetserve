9e3f49aeb293fbc017f82638b6918a7c2704013e
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-04 04:52:27 +0000

    added another test for HttpRequestResponse and pushed the HTTP generation into a helper method to help with the readability of the actual tests.

--- UnitTests/HttpRequestResponseTests.cs
+++ UnitTests/HttpRequestResponseTests.cs
@@ -12,17 +12,32 @@ namespace UnitTests {
 
         [Test]
         public void Simple_Get_Request_Test() {
-            string httpRequest =
-                @"GET / HTTP/1.1
-Host: Localhost
-
-";
-            MemoryStream stream = new MemoryStream(Encoding.ASCII.GetBytes(httpRequest));
+            MemoryStream stream = GetRequestStream("GET / HTTP/1.1", "Host: Localhost", "");
             HttpRequestResponse httpRequestResponse = new HttpRequestResponse(stream, null, null);
 
             Assert.AreEqual(httpRequestResponse.Request.HttpMethod, "GET");
             Assert.AreEqual(httpRequestResponse.Request.RawUrl, "/");
             Assert.IsFalse(httpRequestResponse.Request.IsKeepAlive);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception), ExpectedMessage = "Error parsing request headers.")]
+        public void Simple_Get_Request_Lacking_Host_Header_Test() {
+            //This test should throw an exception as the Host header is required.
+            //Ideally we would be able to better test for this, but for now all we
+            //can do is test for the above generic exception.
+            MemoryStream stream = GetRequestStream("GET / HTTP/1.1", "");
+            HttpRequestResponse httpRequestResponse = new HttpRequestResponse(stream, null, null);
+        }
+
+        private MemoryStream GetRequestStream(params string[] lines) {
+            MemoryStream result = new MemoryStream();
+            foreach(string line in lines) {
+                byte[] rawData = Encoding.ASCII.GetBytes(string.Format("{0}\r\n", line));
+                result.Write(rawData, 0, rawData.Length);
+            }
+            result.Position = 0;
+            return result;
         }
     }
 }
