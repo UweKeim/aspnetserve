1e9a4bd28ee79b00fdb0cfa78de6383749766856
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-09 03:32:19 +0000

    initial addition of aspNETserve.Ice, the aspNETserve windows service.

---
+++ aspNETserve.Ice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace aspNETserve.Ice {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main() {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service1() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ aspNETserve.Ice/Service1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace aspNETserve.Ice {
+    public partial class Service1 : ServiceBase {
+        public Service1() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+        }
+
+        protected override void OnStop() {
+        }
+    }
+}
