91b7efd0240320ff184ef641f5b7ceb567f4294b
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-12 03:19:41 +0000

    initial addition of the XmlConfigurationManager and the Xml sub-namespace of aspNETserve.Configuration

---
+++ aspNETserve/Configuration/Xml/XmlConfigurationManager.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************
+ * Copyright (c) 2006-2008, Jason Whitehorn (jason.whitehorn@gmail.com)
+ * All rights reserved.
+ * 
+ * Source code and binaries distributed under the terms of the included
+ * license, see license.txt for details.
+ ************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace aspNETserve.Configuration.Xml {
+    public class XmlConfigurationManager {
+
+        public IApplication FromXml(Stream stream) {
+            throw new NotImplementedException();
+        }
+    }
+}
