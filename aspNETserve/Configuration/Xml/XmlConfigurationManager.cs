f2e1eef468a39d8a6705b2a0c82a02a74f613883
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-13 04:29:52 +0000

    completed the initial implementation of the FromXml method on the XmlConfigurationManager object.

--- aspNETserve/Configuration/Xml/XmlConfigurationManager.cs
+++ aspNETserve/Configuration/Xml/XmlConfigurationManager.cs
@@ -8,13 +8,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
 
 namespace aspNETserve.Configuration.Xml {
-    public class XmlConfigurationManager {
+    public static class XmlConfigurationManager {
         private static readonly XmlSchema _xmlSchema;
 
         static XmlConfigurationManager() {
@@ -25,13 +26,45 @@ namespace aspNETserve.Configuration.Xml {
             }
         }
 
-        public static IApplication FromXml(Stream stream) {
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.Schemas.Add(_xmlSchema);
-            XmlReader xmlReader = XmlReader.Create(stream, settings);
+        public static IConfiguration FromXml(Stream stream) {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(stream);
 
-            
-            throw new NotImplementedException();
+            xDoc.Schemas.Add(_xmlSchema);
+            xDoc.Validate(null);    //asert that the document confirms to the XSD
+
+            Configuration config = new Configuration(new List<IApplication>());
+
+            XmlElement serverElement = xDoc.DocumentElement;    //load the root element, named "server".
+            foreach(XmlNode applicationElement in serverElement.ChildNodes) {   //loop over each "application" element...
+
+                string physicalPath = applicationElement.Attributes["physicalPath"].Value; 
+                //construct an IApplcation to represent this XmlNode
+                Application app = new Application(physicalPath, new List<IDomain>(), new List<IEndPoint>());
+
+                foreach(XmlNode childNode in applicationElement.ChildNodes) {
+                    //the "application" element can either contain "domain" or "endpoint" elements, so...
+
+                    switch (childNode.Name) {    //...first figure out which it it
+                        case "domain":
+                            Domain domain = new Domain(childNode.Attributes["name"].Value, childNode.Attributes["virtualPath"].Value);
+                            app.Domains.Add(domain);
+                            break;
+                        case "endpoint":
+                            IPAddress ip = IPAddress.Parse(childNode.Attributes["ip"].Value);
+                            int port = int.Parse(childNode.Attributes["port"].Value);
+                            //since the "secure" attribute is optional we need to check for null first.
+                            bool isSecure = childNode.Attributes["secure"] == null ? false : bool.Parse(childNode.Attributes["secure"].Value);
+                            EndPoint endPoint = new EndPoint(ip, port, isSecure);
+                            app.EndPoints.Add(endPoint);
+                            break;
+                    }
+                }
+
+                config.Applications.Add(app);   //finally add the IApplication to the running tally on the configuration object.
+            }
+
+            return config;
         }
 
     }
