42dc2c0621546dffea62fb2d6d2e64bed612ade5
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-13 02:53:47 +0000

    some initial thoughts on the XmlConfigurationManager class

--- aspNETserve/Configuration/Xml/XmlConfigurationManager.cs
+++ aspNETserve/Configuration/Xml/XmlConfigurationManager.cs
@@ -8,15 +8,31 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
 
 namespace aspNETserve.Configuration.Xml {
     public class XmlConfigurationManager {
+        private static readonly XmlSchema _xmlSchema;
 
-        public IApplication FromXml(Stream stream) {
+        static XmlConfigurationManager() {
+            Assembly currentAsm = Assembly.GetExecutingAssembly();
+            using (Stream s = currentAsm.GetManifestResourceStream("aspNETserve.Configuration.Xml.aspNETserve.config.xsd")) {
+                _xmlSchema = XmlSchema.Read(s, null);
+                s.Close();
+            }
+        }
+
+        public static IApplication FromXml(Stream stream) {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(_xmlSchema);
+            XmlReader xmlReader = XmlReader.Create(stream, settings);
+
+            
             throw new NotImplementedException();
         }
+
     }
 }
