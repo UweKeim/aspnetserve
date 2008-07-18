8f7933bd015aa8273b36a6abaf91b5aaabf8fb77
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-18 04:44:52 +0000

    implemented the basic structure of the WepApplicationPackage class, all that remains is the core methods of ExtractWapToPath and DeleteExtractedArchive.

--- aspNETserve/WebApplicationPackage.cs
+++ aspNETserve/WebApplicationPackage.cs
@@ -1,29 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace aspNETserve {
+    /// <summary>
+    /// Represents a WebApplicationPackage archive.
+    /// </summary>
     public class WebApplicationPackage : IDisposable {
+        private bool _isOpen;
+        private bool _ownsStream;
+        private Stream _stream;
+        private string _physicalPath;
+
+        /// <summary>
+        /// Constructs an instance of WebApplicationPackage.
+        /// This constructor assumes ownership of the stream, and will
+        /// close it when the WebApplicationPackage is disposed.
+        /// </summary>
+        /// <param name="stream">A Stream containing the WAP archive.</param>
+        public WebApplicationPackage(Stream stream) : this(stream, true) {}
+
+        /// <summary>
+        /// Constructs an instance of WebApplicationPackage.
+        /// </summary>
+        /// <param name="stream">A Stream containing the WAP archive.</param>
+        /// <param name="ownsStream">If true the stream will be closed when the 
+        /// instance of WebApplicationPackage is disposed, otherwise it will be left open.
+        /// </param>
+        public WebApplicationPackage(Stream stream, bool ownsStream) {
+            _stream = stream;
+            _ownsStream = ownsStream;
+        }
 
         /// <summary>
         /// releases the unmanaged resources allocated by the current
         /// WebApplicationPackage instance.
         /// </summary>
-        public void Dispose(){}
+        public virtual void Dispose() {
+            if(_ownsStream)
+                _stream.Close();
+            if(IsOpen)
+                DeleteExtractedArchive();
+            IsOpen = false;
+        }
 
         /// <summary>
         /// Opens the WebApplicationPackage for reading.
         /// </summary>
-        public void Open(){}
+        public virtual void Open() {
+            if(IsOpen)
+                throw new Exception("Cannot open WAP, it is already opened.");
+            IsOpen = true;
+            string directory = (new Guid()).ToString();
+            _physicalPath = Path.Combine(Path.GetTempPath(), directory);
+            ExtractWapToPath(_physicalPath);
+        }
 
         /// <summary>
         /// Returns the path on disk to the contents
         /// of an opened WebApplicationPackage.
         /// </summary>
-        public string PhysicalPath {
+        public virtual string PhysicalPath {
             get {
-                throw new NotImplementedException();
+                if(!IsOpen)
+                    throw new Exception("The WAP is not opened yet.");
+                return _physicalPath;
             }
+        }
+
+        /// <summary>
+        /// Determines if the WebApplicationPackage has been opened
+        /// </summary>
+        public virtual bool IsOpen {
+            get { return _isOpen; }
+            protected set { _isOpen = value; }
+        }
+
+        protected virtual void ExtractWapToPath(string path) {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void DeleteExtractedArchive() {
+            throw new NotImplementedException();
         }
     }
 }
