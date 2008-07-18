d08695fb4fdbcd015ce67cb80cae1e3c732415f0
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-07-18 04:14:10 +0000

    added the WebApplicationPackage class, and outlined its basic operations.

---
+++ aspNETserve/WebApplicationPackage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aspNETserve {
+    public class WebApplicationPackage : IDisposable {
+
+        /// <summary>
+        /// releases the unmanaged resources allocated by the current
+        /// WebApplicationPackage instance.
+        /// </summary>
+        public void Dispose(){}
+
+        /// <summary>
+        /// Opens the WebApplicationPackage for reading.
+        /// </summary>
+        public void Open(){}
+
+        /// <summary>
+        /// Returns the path on disk to the contents
+        /// of an opened WebApplicationPackage.
+        /// </summary>
+        public string PhysicalPath {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
