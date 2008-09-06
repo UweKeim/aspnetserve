f52d344b9dbc5b29a9d888c426bac42b1a79e708
jason.whitehorn <jason.whitehorn@2026342b-3050-0410-bf77-bbce3f982d02>
2008-09-06 02:39:34 +0000

    initial addition of a custom logging namespace to ease the burden of cross AppDomain logging.

---
+++ aspNETserve.Core/Logging/LogLevel.cs
@@ -0,0 +1,20 @@
+﻿/************************************************************************
+ * Copyright (c) 2006-2008, Jason Whitehorn (jason.whitehorn@gmail.com)
+ * All rights reserved.
+ * 
+ * Source code and binaries distributed under the terms of the included
+ * license, see license.txt for details.
+ ************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aspNETserve.Core.Logging {
+    public enum LogLevel {
+        Debug = 0,
+        Info = 50,
+        Warning = 100,
+        Error = 150,
+        Fatal = 200
+    }
+}
---
+++ aspNETserve.Core/Logging/Logger.cs
@@ -0,0 +1,41 @@
+﻿/************************************************************************
+ * Copyright (c) 2006-2008, Jason Whitehorn (jason.whitehorn@gmail.com)
+ * All rights reserved.
+ * 
+ * Source code and binaries distributed under the terms of the included
+ * license, see license.txt for details.
+ ************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aspNETserve.Core.Logging {
+    public static class Logger {
+        private static event LogExceptionEventHandler _logExceptionEvent;
+        private static event LogMessageEventHandler _logMessageEvent;
+
+        public static void LogException(LogLevel level, string message, Exception ex) {
+            if (_logExceptionEvent != null)
+                _logExceptionEvent(level, message, ex);
+        }
+
+        public static void LogException(LogLevel level, Exception ex) {
+            LogException(level, string.Empty, ex);
+        }
+
+        public static void LogMessage(LogLevel level, string message) {
+            if (_logMessageEvent != null)
+                _logMessageEvent(level, message);
+        }
+
+        public static event LogExceptionEventHandler LogExceptionEvent {
+            add { _logExceptionEvent += value; }
+            remove { _logExceptionEvent -= value; }
+        }
+
+        public static event LogMessageEventHandler LogMessageEvent {
+            add { _logMessageEvent += value; }
+            remove { _logMessageEvent -= value; }
+        }
+    }
+}
